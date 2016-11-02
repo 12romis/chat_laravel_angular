1f5b5d581af98709f9381c112569160c315f624e
Romis <12romis@gmail.com>
2016-11-02 14:13:53 +0200

    angular first

---
+++ public/app/scripts/app.js
@@ -0,0 +1,23 @@
+/**
+ * Created by Romis on 02.11.16.
+ */
+
+
+'use strict';
+
+angular.module('chatApp', ['ngRoute'])
+
+    .config(function($routeProvider){
+
+        $routeProvider
+
+            .when('/chat-rooms', {
+                templateUrl: '/partials/chat-rooms.html',
+                controller:  'ChatRoomsCtrl'
+            })
+
+            .when('/chat-room', {
+                templateUrl: '/partials/chat-room.html',
+                controller:  'ChatRoomCtrl'
+            });
+    });
---
+++ public/app/scripts/controllers/chat-room.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Romis on 02.11.16.
+ */
+
+'use strict';
+
+angular.module('chatApp')
+
+    .controller('ChatRoomCtrl', function ($scope) {
+
+    });
